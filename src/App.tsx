cb08fc7402b9cf666dda5d6a6da7e73fd9644e10
Kalucza Gábor <kaluczadzsi@gmail.com>
2024-04-07 12:17:15 +0200

    applying css styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Content from './Content';
+import Footer from './Footer';
 import Header from './Header';
 
 const App = () => {
@@ -6,6 +7,7 @@ const App = () => {
     <div className="App">
       <Header />
       <Content />
+      <Footer />
     </div>
   );
 };
