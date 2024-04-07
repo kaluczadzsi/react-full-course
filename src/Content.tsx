0243ce7a54bab49bde5197de1b8b582edb7479ba
Kalucza Gábor <kaluczadzsi@gmail.com>
2024-04-07 13:45:52 +0200

    useState hook

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,16 +1,26 @@
-import { MouseEventHandler, useState } from 'react';
+import { useState } from 'react';
 
 const Content = () => {
-  const [name, setName] = useState('');
+  const [name, setName] = useState('Kate');
+  const [count, setCount] = useState(0);
+
   const handleNameChange = () => {
     const names = ['Tom', 'Brandon', 'Bruce', 'Dave', 'Mike'];
     const random = Math.floor(Math.random() * names.length);
     setName(names[random]);
   };
 
+  const handleClick = () => {
+    setCount((prevState) => prevState + 2);
+    setCount((prevState) => prevState + 2);
+    console.log(count);
+  };
+
   return (
     <main>
-      <p onDoubleClick={handleNameChange}>Hello {name}</p>
+      <p>Hello {name}!</p>
+      <button onClick={handleNameChange}>Change Name</button>
+      <button onClick={handleClick}>Click</button>
     </main>
   );
 };
